cbcea7281d481cb31a108b98ab47fc5966d024ca
Sergio Cerda <sergio.cerda.lozano@gmail.com>
2024-04-06 01:59:24 -0300

    Avance seccion 21 - 291

--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+
+(mapboxgl as any).accessToken = 'pk.eyJ1Ijoic3B5a3VnIiwiYSI6ImNsdW5heDltajFsaDIybWxob2lzdHkxNzAifQ.mH8ANOGY22b6flamgHKB0w';
 
 @Component({
   templateUrl: './full-screen-page.component.html',
   styleUrl: './full-screen-page.component.css'
 })
-export class FullScreenPageComponent {
+export class FullScreenPageComponent implements AfterViewInit{
+
+  ngAfterViewInit(): void {
+
+    const map = new mapboxgl.Map({
+      container: 'map', // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-70.14027, -20.25800], // starting position [lng, lat]
+      zoom: 12, // starting zoom
+    });
+
+  }
+
+
 
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+
+export const environment = {
+  mapbox_key: "pk.eyJ1Ijoic3B5a3VnIiwiYSI6ImNsdW5iM3NwNjBpaXoya29reTJxeDV3eGcifQ.jlvHLvjm7VzwqXOLxGr2VA",
+  otra: "Propiedad"
+};
